a4b32e5e8e15c4798adb2663dd9702a4a6610013
LCbalsa <104496023+LCbalsa@users.noreply.github.com>
2025-09-10 02:19:20 +0800

    [style] replace banners

--- public/banner/banner.ts
+++ public/banner/banner.ts
@@ -3,4 +3,6 @@ export const banners = [
     { label: 'banner_2', img: '/banner/banner2.png' },
     { label: 'banner_3', img: '/banner/banner3.png' },
     { label: 'banner_4', img: '/banner/banner4.png' },
+    { label: 'banner_5', img: '/banner/banner5.png' },
+    { label: 'banner_6', img: '/banner/banner6.png' },
 ];
