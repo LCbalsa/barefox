0a182e191c3e65740911d55b0e6e95542a77cf1b
LCbalsa <104496023+LCbalsa@users.noreply.github.com>
2025-09-10 17:13:16 +0800

    [style] featured items in home

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -2,10 +2,8 @@ import { ProductList } from "@/components/product-list";
 import { stripe } from "@/lib/stripe";
 import Banner from "@/components/ui/banner"
 import CategoryCard from "@/components/ui/category";
-import { Carousel } from "@/components/carousel";
-import { brands } from "@/public/brand/brand";
-import Image from "next/image";
 import Brand from "@/components/ui/brand";
+import { Carousel } from "@/components/carousel";
 
 export default async function ProductsPage() {
   const products = await stripe.products.list({
@@ -17,6 +15,12 @@ export default async function ProductsPage() {
       <Banner />
       <CategoryCard />
       <Brand />
+      <h1 className="text-2xl font-light leading-none tracking-tight text-foreground text-center mb-8 pt-10">
+        Featured <span className="text-orange-500">Items</span>
+      </h1>
+      <section className="py-8 bg-white">
+        <Carousel products={products.data} />
+      </section>
 
       <h1 className="text-2xl font-light leading-none tracking-tight text-foreground text-center mb-8 pt-10">
         All <span className="text-orange-500">Products</span>
