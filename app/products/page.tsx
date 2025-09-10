0f0ffbd0330d82d58a8ca762a4036cf5b7d7701c
LCbalsa <104496023+LCbalsa@users.noreply.github.com>
2025-09-10 15:38:59 +0800

    [style] add brand section

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -2,6 +2,10 @@ import { ProductList } from "@/components/product-list";
 import { stripe } from "@/lib/stripe";
 import Banner from "@/components/ui/banner"
 import CategoryCard from "@/components/ui/category";
+import { Carousel } from "@/components/carousel";
+import { brands } from "@/public/brand/brand";
+import Image from "next/image";
+import Brand from "@/components/ui/brand";
 
 export default async function ProductsPage() {
   const products = await stripe.products.list({
@@ -12,7 +16,9 @@ export default async function ProductsPage() {
     <div className="pb-8">
       <Banner />
       <CategoryCard />
-      <h1 className="text-3xl font-bold leading-none tracking-tight text-foreground text-center mb-8 pt-10">
+      <Brand />
+
+      <h1 className="text-2xl font-light leading-none tracking-tight text-foreground text-center mb-8 pt-10">
         All <span className="text-orange-500">Products</span>
       </h1>
       <ProductList products={products.data} />
