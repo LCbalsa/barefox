ad6a3ed97760e45398338a7dd346279a367b6352
LCbalsa <104496023+LCbalsa@users.noreply.github.com>
2025-09-12 01:17:36 +0800

    [style] fix highlight responsiveness

--- components/ui/highlight.tsx
+++ components/ui/highlight.tsx
@@ -7,7 +7,7 @@ import { motion } from 'motion/react'
 const Highlight = () => {
   return (
     <div className='grid lg:grid-cols-2 sm:grid-cols-1'>
-        <div className='relative w-full h-[560px] group overflow-hidden'>
+        <div className='relative w-full lg:h-[560px] h-[350px] group overflow-hidden'>
             {/* Reveals image as initial background */}
             <motion.div initial={{ opacity:1 }} whileHover={{ opacity:0 }} transition={{ duration: 0.5 }}
             className='absolute inset-0 z-0'>
@@ -17,16 +17,17 @@ const Highlight = () => {
 
             {/* Product description overlay */}
             <motion.div initial={{ opacity:0 }} whileHover={{ opacity:1 }} transition={{ duration:0.5 }} 
-            className='absolute inset-0 z-10 flex flex-col bg-gradient-to-r from-black to-white/10 text-white pl-10 pt-10'>
-                <h1 className='lg:text-5xl font-bold'>Valour</h1>
+            className='absolute inset-0 z-10 flex flex-col bg-gradient-to-r from-black to-white/10 text-white 
+            pl-2 lg:pl-10 pt-2 lg:pt-10'>
+                <h1 className='lg:text-5xl text-4xl font-bold'>Valour</h1>
                 <p className='mt-4'>Drizzle Short-cut Stiletto Leather Boots</p>
-                <p className='py-15 text-xs lg:w-[300px]'>This is the description of the clothing article 
+                <p className='py-15 text-xs lg:w-[300px] w-[200px]'>This is the description of the clothing article 
                     This is the description of the clothing article This is the description of the clothing article
                 </p>
-                <button className='bg-white p-2 text-black w-40 absolute bottom-20'>View Product</button>
+                <button className='bg-white p-2 text-black w-40 absolute lg:bottom-20 bottom-5'>View Product</button>
             </motion.div>
         </div>
-        <div className='relative w-full h-[560px] group overflow-hidden'>
+        <div className='relative w-full lg:h-[560px] h-[350px] group overflow-hidden'>
             {/* Reveals image as initial background */}
             <motion.div initial={{ opacity:1 }} whileHover={{ opacity:0 }} transition={{ duration: 0.5 }}
             className='absolute inset-0 z-0'>
@@ -36,13 +37,13 @@ const Highlight = () => {
 
             {/* Product description overlay */}
             <motion.div initial={{ opacity:0 }} whileHover={{ opacity:1 }} transition={{ duration:0.5 }} 
-            className='absolute inset-0 z-10 flex flex-col text-white p-10 bg-gradient-to-r from-black to-white/10'>
-                <h1 className='lg:text-5xl font-bold'>Chikota</h1>
+            className='absolute inset-0 z-10 flex flex-col text-white lg:p-10 p-5 bg-gradient-to-r from-black to-white/10'>
+                <h1 className='lg:text-5xl text-4xl font-bold'>Chikota</h1>
                 <p className='mt-4'>Finesse Flat Bottom Mid-top Leather Martin Boots</p>
-                <p className='py-15 text-xs lg:w-[300px]'>This is the description of the clothing article 
+                <p className='py-15 text-xs lg:w-[300px] w-[200px]'>This is the description of the clothing article 
                     This is the description of the clothing article This is the description of the clothing article
                 </p>
-                <button className='bg-white p-2 text-black w-40 absolute bottom-20'>View Product</button>
+                <button className='bg-white p-2 text-black lg:w-40 sm:w-30 absolute lg:bottom-20 bottom-5'>View Product</button>
             </motion.div>
         </div>
     </div>
