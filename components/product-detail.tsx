71a5a5b6fdd6d7aab1bf1ef2a92bcf5df812f03d
LCbalsa <104496023+LCbalsa@users.noreply.github.com>
2025-09-26 10:55:10 +0800

    [style] product detail rework

--- components/product-detail.tsx
+++ components/product-detail.tsx
@@ -5,9 +5,10 @@ import Stripe from "stripe";
 import Image from "next/image";
 import { Button } from "@/components/ui/button"; // Shadcn Button
 import { useCartStore } from "@/store/cart-store";
-import { useState, useEffect } from "react";
+import { useState, useEffect, SetStateAction } from "react";
 import { createClient } from "@/lib/supabase/client";
 import { useRouter } from "next/navigation";
+import { BsHeart } from "react-icons/bs";
 
 interface Props {
   product: Stripe.Product;
@@ -55,10 +56,16 @@ export const ProductDetail = ({ product }: Props) => {
     setSelectedQuantity(1); // reset after adding
   };
 
+  const [sizeSelect, setSizeSelect] = useState('S');
+
+  const handleSizeSelect = (size: SetStateAction<string>) => {
+    setSizeSelect(size);
+  };
+
   return (
-    <div className="container mx-auto px-4 py-8 flex flex-col md:flex-row gap-8 items-center">
+    <div className="container mx-auto px-4 lg:py-8 py-2 flex flex-col md:flex-row gap-8 items-center">
       {product.images && product.images[0] && (
-        <div className="relative h-96 w-full md:w-1/2 rounded-lg overflow-hidden">
+        <div className="relative lg:h-160 h-100 w-full md:w-1/2 overflow-hidden">
           <Image
             src={product.images[0]}
             alt={product.name}
@@ -68,39 +75,61 @@ export const ProductDetail = ({ product }: Props) => {
         </div>
       )}
       <div className="md:w-1/2">
-        <h1 className="text-3xl font-bold mb-4 text-orange-600">
+        <h1 className="text-3xl font-light mb-4">
           {product.name}
         </h1>
-        {product.description && (
+        {/* {product.description && (
           <p className="text-gray-700 mb-4">{product.description}</p>
-        )}
+        )} */}
         {price && price.unit_amount && (
-          <p className="text-lg font-semibold text-orange-500">
+          <p className="text-lg font-bold">
             ${(price.unit_amount / 100).toFixed(2)}
           </p>
         )}
+
+        <p className="text-gray-400 text-xs">(Additional fee may apply upon checkout)</p>
 
         {/* Quantity Selector */}
         <div className="flex items-center space-x-4 mt-4 mb-4">
           <Button
             variant="outline"
-            className="border-orange-400 text-orange-400 hover:bg-orange-50 hover:border-orange-500"
+            className="border-gray-400 text-black-400 hover:bg-gray-200 hover:border-gray-500"
             onClick={decrement}
           >
             –
           </Button>
           <span className="text-lg font-semibold">{selectedQuantity}</span>
           <Button
-            className="bg-orange-500 hover:bg-orange-600 text-white"
+            className="bg-gray-900 hover:bg-black text-white"
             onClick={increment}
           >
             +
           </Button>
+
+        </div>
+
+        {/* Size Selector */}
+        <div className="mt-10 mb-10">
+          <h1 className="text-sm text-gray-500">Select Size</h1>
+            <ul className="flex flex-wrap w-full text-sm font-light pl-5 pt-2 text-center gap-2 sm:gap-4 lg:gap-5">
+              {['XS', 'S', 'M', 'L', 'XL', '2XL', '3XL', '4XL'].map((size) => (
+                <li
+                  key={size}
+                  className={`lg:p-3 p-4 cursor-pointer ${
+                    size === sizeSelect ? 'bg-gray-400/10 rounded-sm font-medium' : 'text-black'
+                  }`}
+                  onClick={() => handleSizeSelect(size)}
+                >
+                  {size}
+                </li>
+              ))}
+            </ul>
         </div>
 
         {/* Add to Cart Button */}
         <Button
-          className="bg-orange-500 hover:bg-orange-600 text-white py-5 mt-2 text-1xl"
+          className="border-1 border-gray-600 bg-white font-light text-gray-900 hover:bg-black hover:text-white
+          mt-2 text-md p-6 w-full lg:w-3/4"
           onClick={onAddToCart}
         >
           Add to Cart {cartQuantity > 0 && `(${cartQuantity} in cart)`}
